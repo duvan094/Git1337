68edd0d63d262f6ccfeeae998c9598f2a5e35204
Jacob <jacobduvander@gmail.com>
2015-02-16 16:03:12 +0100

    la till grejer

---
+++ RoligtProgram.java
@@ -0,0 +1,16 @@
+public class RoligtProgram{
+
+	public static void main(String[] args){
+		
+		RoligtProgram program = new RoligtProgram();
+
+	}
+
+	public RoligtProgram(){
+		for(int i = 0; i<20; i++){
+			System.out.println("Rad nr. " + i);
+		}
+
+	}
+
+}
